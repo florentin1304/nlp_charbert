d4fcdabab8695f2c84e6fb351887f0e6c44dd57b
Claudio Savelli <claudiosav2000@gmail.com>
2024-02-13 17:50:57 +0100

    fixed medical_ner

--- Datasets/load_medical_ner.py
+++ Datasets/load_medical_ner.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import os 
 
 dataset = load_dataset("jnlpba")
 
@@ -10,4 +11,35 @@ test = val_test.shard(2, 1)
 dataset['validation'] = val
 dataset['test'] = test
 
-dataset.save_to_disk('dataset/medical_domain/ner')
+int_to_key = dataset['train'].features["ner_tags"].feature.int2str
+
+path = 'datasets/medical_domain/ner/'
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+with open('train.txt', 'w') as f:
+    all_tokens = dataset['train']['tokens']
+    all_tags = dataset['train']['ner_tags']
+    for tokens, tags in zip(all_tokens, all_tags):
+        for token, tag in zip(tokens, tags):
+            f.write(f'{token} {int_to_key(tag)}\n')
+        f.write('\n')
+
+# create a txt file for validation set
+with open('val.txt', 'w') as f:
+    all_tokens = dataset['validation']['tokens']
+    all_tags = dataset['validation']['ner_tags']
+    for tokens, tags in zip(all_tokens, all_tags):
+        for token, tag in zip(tokens, tags):
+            f.write(f'{token} {int_to_key(tag)}\n')
+        f.write('\n')
+
+# create a txt file for test set
+with open('test.txt', 'w') as f:
+    all_tokens = dataset['test']['tokens']
+    all_tags = dataset['test']['ner_tags']
+    for tokens, tags in zip(all_tokens, all_tags):
+        for token, tag in zip(tokens, tags):
+            f.write(f'{token} {int_to_key(tag)}\n')
+        f.write('\n')
