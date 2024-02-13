88d26a05765f4e64b7b52162987f085d729598dd
Florentin-Cristian Udrea <100294063+florentin1304@users.noreply.github.com>
2024-02-13 17:25:00 +0100

    Started refactoring code

---
+++ datasets/load_medical_ner.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+
+dataset = load_dataset("jnlpba")
+
+# divide DatasetDict into train, validation, and test
+val_test = dataset['validation']
+val = val_test.shard(2, 0)
+test = val_test.shard(2, 1)
+
+dataset['validation'] = val
+dataset['test'] = test
+
+dataset.save_to_disk('dataset/medical_domain/ner')
---
+++ model/download_biomednlp_bert.bash
@@ -0,0 +1,6 @@
+cd model
+mkdir input
+cd input
+git clone https://huggingface.co/microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext
+cd ..
+cd ..
