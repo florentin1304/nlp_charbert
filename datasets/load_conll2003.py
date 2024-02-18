8f34d9ec6daac474d532c3536921e94c2755e079
LucaCatalano13 <luca.catalano1307@gmail.com>
2024-02-18 12:45:21 +0100

    Update load_conll2003.py

--- datasets/load_conll2003.py
+++ datasets/load_conll2003.py
@@ -2,6 +2,6 @@ import wget
 import os
 
 os.makedirs(os.path.dirname("./CoNLL2003/"), exist_ok=True)
-train_file = wget.download("https://github.com/pranabsarkar/Conll_task/blob/master/conll-2003/eng.train", "./CoNLL2003/train.txt")
-val_file = wget.download("https://github.com/pranabsarkar/Conll_task/blob/master/conll-2003/eng.testa", "./CoNLL2003/val.txt")
-test_file = wget.download("https://github.com/pranabsarkar/Conll_task/blob/master/conll-2003/eng.testb", "./CoNLL2003/test.txt")
+train_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/train.txt", "./CoNLL2003/train.txt")
+val_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/valid.txt", "./CoNLL2003/val.txt")
+test_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/test.txt", "./CoNLL2003/test.txt")
