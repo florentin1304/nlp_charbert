c55a6e21caf2da1a9a5997fd7daedad7eb132ccc
Florentin-Cristian Udrea <100294063+florentin1304@users.noreply.github.com>
2024-02-22 15:10:58 +0100

    Changes to run_domain_shift mainly

--- datasets/load_conll2003.py
+++ datasets/load_conll2003.py
@@ -1,7 +1,9 @@
 import wget
 import os
 
-os.makedirs(os.path.dirname("./CoNLL2003/"), exist_ok=True)
-train_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/train.txt", "./CoNLL2003/train.txt")
-val_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/valid.txt", "./CoNLL2003/val.txt")
-test_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/test.txt", "./CoNLL2003/test.txt")
+os.makedirs(os.path.dirname("./datasets/CoNLL2003/"), exist_ok=True)
+
+train_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/train.txt", "./datasets/CoNLL2003/train.txt")
+val_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/valid.txt", "./datasets/CoNLL2003/val.txt")
+val_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/valid.txt", "./datasets/CoNLL2003/dev.txt")
+test_file = wget.download("https://raw.githubusercontent.com/chnsh/BERT-NER-CoNLL/master/data/test.txt", "./datasets/CoNLL2003/test.txt")
